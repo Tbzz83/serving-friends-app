722a96dfd4493142f79fcc1650b0119c5d5c70e2
Tobi Azeez <azeezoe@appstate.edu>
2024-12-01 11:56:40 -0500

    updated code

--- tf_backend/permissions.tf
+++ tf_backend/permissions.tf
@@ -9,19 +9,8 @@ data "azurerm_role_definition" "storage_account_contributor" {
   name = "Storage Account Contributor"
 }
 
-data "azurerm_storage_account" "tfstate_store" {
-  name                = "tfstatestorebio"
-  resource_group_name = "tfstate_rg"
-}
-
-#resource "azurerm_role_assignment" "storage_account_contributor" {
-#  scope                = data.azurerm_storage_account.tfstate_store.id
-#  role_definition_name = "Storage Account Contributor"
-#  principal_id         = data.azuread_client_config.current.object_id
-#}
-
 resource "azurerm_role_assignment" "storage_blob_data_contributor" {
-  scope                = data.azurerm_storage_account.tfstate_store.id
+  scope                = data.azurerm_resource_group.education.id
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = data.azuread_client_config.current.object_id
 }
