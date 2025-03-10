8d835fe98c6dfd39b72363c7568a6f1380ec0a5c
Tobi Azeez <azeezoe@appstate.edu>
2025-03-09 21:36:45 -0400

    Updated for consistency in project

--- serving_from_web_app/infra/modules/app/main.tf
+++ serving_from_web_app/infra/modules/app/main.tf
@@ -6,6 +6,10 @@ resource "azurerm_static_web_app" "app-ui" {
   sku_tier            = var.static_app_sku_tier
   sku_size            = var.static_app_sku_size
   public_network_access_enabled = var.ui_public_access
+
+  tags = {
+    environment = var.tag
+  }
 }
 
 
@@ -60,12 +64,18 @@ resource "azurerm_linux_web_app" "app-api" {
 
   app_settings = {
     "WEBSITES_PORT" = "5000"
+    "WEBSITE_WEBDEPLOY_USE_SCM" = true
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = 1
   }
 
   site_config {
     application_stack {
       python_version = "3.12"
     }
+  }
+
+  tags = {
+    environment = var.tag
   }
 }
 
@@ -79,36 +89,47 @@ resource "azurerm_linux_web_app_slot" "staging" {
   # Enable below for VNet integration
   virtual_network_subnet_id = var.api_app_subnet.id
 
-  auth_settings_v2 {
-    unauthenticated_action = "RedirectToLoginPage"
-    auth_enabled = true
-    forward_proxy_convention = "NoProxy"
-    http_route_api_prefix = "/.auth"
-    require_authentication = true
-    require_https = true
-    runtime_version = "~1"
-    login {
-      cookie_expiration_convention = "FixedTime"
-      cookie_expiration_time = "08:00:00"
-      nonce_expiration_time = "00:05:00"
-      preserve_url_fragments_for_logins = false
-      token_refresh_extension_time = 72
-      token_store_enabled = false
-      validate_nonce = true
-    }
-
-    azure_static_web_app_v2 {
-      client_id = azurerm_static_web_app.app-ui.default_host_name 
-    }
-  }
+# We can leave out the auth settings for the staging slot so we can 
+# verify our app is working by directly heading to <url>/api/friends
+# If authentication is enabled like below then only the static web app can
+# access our api
+  
+#  auth_settings_v2 {
+#    unauthenticated_action = "RedirectToLoginPage"
+#    auth_enabled = true
+#    forward_proxy_convention = "NoProxy"
+#    http_route_api_prefix = "/.auth"
+#    require_authentication = true
+#    require_https = true
+#    runtime_version = "~1"
+#    login {
+#      cookie_expiration_convention = "FixedTime"
+#      cookie_expiration_time = "08:00:00"
+#      nonce_expiration_time = "00:05:00"
+#      preserve_url_fragments_for_logins = false
+#      token_refresh_extension_time = 72
+#      token_store_enabled = false
+#      validate_nonce = true
+#    }
+#
+#    azure_static_web_app_v2 {
+#      client_id = azurerm_static_web_app.app-ui.default_host_name 
+#    }
+#  }
 
   app_settings = {
     "WEBSITES_PORT" = "5000"
+    "WEBSITE_WEBDEPLOY_USE_SCM" = true
+    "SCM_DO_BUILD_DURING_DEPLOYMENT" = 1
   }
 
   site_config {
     application_stack {
       python_version = "3.12"
     }
+  }
+
+  tags = {
+    environment = var.tag
   }
 }
